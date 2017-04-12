8e20f6fdc4fd473bae0d71b9ea67433847a37f0d
Sagar Kudale <sagar.kudale@cuelogic.co.in>
2017-04-12 13:07:56 +0530

    Initial Commit

---
+++ VSMobileCenterDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VSMobileCenterDemo
+//
+//  Created by Mayur Sojrani on 12/04/17.
+//  Copyright © 2017 Com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VSMobileCenterDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VSMobileCenterDemo
+//
+//  Created by Mayur Sojrani on 12/04/17.
+//  Copyright © 2017 Com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
